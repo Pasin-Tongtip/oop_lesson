fe457d8ea42245e969b5f282bf62ba1248851fcf
Pasin-Tongtip <pasin.to@ku.th>
2024-11-05 13:02:14 +0700

    data_processing.py in procedural style

--- data_processing.py
+++ data_processing.py
@@ -24,13 +24,13 @@ print(sum(temps)/len(temps))
 print()
 
 # Print all cities in Italy
-cities_temp = []
+temps = []
 my_country = 'Italy'
 for city in cities:
     if city['country'] == my_country:
-        cities_temp.append(city['city'])
+        temps.append(city['city'])
 print("All the cities in", my_country, ":")
-print(cities_temp)
+print(temps)
 print()
 
 # Print the average temperature for all the cities in Italy
@@ -42,6 +42,7 @@ for city in cities:
 print("The average temperature of all the cities in", my_country, ":")
 print(sum(temps)/len(temps))
 print()
+# Write code for me
 
 # Print the max temperature for all the cities in Italy
 temps = []
@@ -52,8 +53,45 @@ for city in cities:
 print("The max temperature of all the cities in", my_country, ":")
 print(max(temps))
 print()
+# Write code for me
 
-# Print the min temperature for all the cities in Italy
+# Let's write a function to filter out only items that meet the condition
+def filter(condition, dict_list):
+    filtered_list = []
+    for item in dict_list:
+        if condition(item):
+            filtered_list.append(item)
+    return filtered_list
+    pass
+
+# Let's write a function to do aggregation given an aggregation function and an aggregation key
+def aggregate(aggregation_key, aggregation_function, dict_list):
+    values = []
+    for item in dict_list:
+        values.append(float(item[aggregation_key]))
+    return aggregation_function(values)
+    pass
+
+# Let's write code to
+# - print the average temperature for all the cities in Italy
+temps = []
+my_country = 'Italy'
+for city in cities:
+    if city['country'] == my_country:
+        temps.append(float(city['temperature']))
+print("The average temperature of all the cities in", my_country, ":")
+print(sum(temps)/len(temps))
+print()
+# - print the average temperature for all the cities in Sweden
+temps = []
+my_country = 'Sweden'
+for city in cities:
+    if city['country'] == my_country:
+        temps.append(float(city['temperature']))
+print("The average temperature of all the cities in", my_country, ":")
+print(sum(temps)/len(temps))
+print()
+# - print the min temperature for all the cities in Italy
 temps = []
 my_country = 'Italy'
 for city in cities:
@@ -62,9 +100,12 @@ for city in cities:
 print("The min temperature of all the cities in", my_country, ":")
 print(min(temps))
 print()
-
-# Let's write code to
-# - print the average temperature for all the cities in Italy
-# - print the average temperature for all the cities in Sweden
-# - print the min temperature for all the cities in Italy
 # - print the max temperature for all the cities in Sweden
+temps = []
+my_country = 'Sweden'
+for city in cities:
+    if city['country'] == my_country:
+        temps.append(float(city['temperature']))
+print("The max temperature of all the cities in", my_country, ":")
+print(max(temps))
+print()
